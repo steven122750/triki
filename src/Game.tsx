b555349e184d3485f787ca12db3aaea260ee8cfe
steven122750 <cardona21507@gmail.com>
2024-05-30 16:40:35 -0500

    primer commit

---
+++ src/Game.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from './Board';
+
+const Game: React.FC = () => {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+    </div>
+  );
+};
+
+export default Game;
